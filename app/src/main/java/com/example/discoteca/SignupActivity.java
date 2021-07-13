5a6ed5fb96df69bb39c9b64b9918216166cd8ea6
Maria Burguete Euan <majoburguete@fb.com>
2021-07-12 23:14:41 -0500

    Sign up button onClick listener was created #3

--- app/src/main/java/com/example/discoteca/SignupActivity.java
+++ app/src/main/java/com/example/discoteca/SignupActivity.java
@@ -3,12 +3,23 @@ package com.example.discoteca;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.discoteca.databinding.ActivitySignupBinding;
 
 public class SignupActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ActivitySignupBinding binding = ActivitySignupBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_signup);
+
+        binding.btnNewus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
     }
 }
