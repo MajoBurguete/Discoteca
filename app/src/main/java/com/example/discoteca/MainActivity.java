5785690a41bcc5ea04cde0fd4a61a2bfee5a9798
Maria Burguete Euan <majoburguete@fb.com>
2021-07-12 22:56:44 -0500

    Logout button onClick listener makes the current user null and returns to the login activity #4

--- app/src/main/java/com/example/discoteca/MainActivity.java
+++ app/src/main/java/com/example/discoteca/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.discoteca;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.discoteca.databinding.ActivityMainBinding;
+import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnLogout = findViewById(R.id.btnLogout);
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ParseUser.logOut();
+                ParseUser currentUser = ParseUser.getCurrentUser();
+                Intent login = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(login);
+                finish();
+            }
+        });
     }
 }
