afb12238f340d40e01ed55fe3ba3c3979b0898cf
Maria Burguete Euan <majoburguete@fb.com>
2021-07-12 22:18:35 -0500

    OnClick listener was set for the login button #2

--- app/src/main/java/com/example/discoteca/LoginActivity.java
+++ app/src/main/java/com/example/discoteca/LoginActivity.java
@@ -3,12 +3,31 @@ package com.example.discoteca;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.discoteca.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        ActivityLoginBinding binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = binding.etUsername.getText().toString();
+                String password = binding.etPassword.getText().toString();
+                if ( username.isEmpty() || password.isEmpty()){
+                    Toast.makeText(LoginActivity.this, "Make sure not to leave anything blank!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                // Method to login
+            }
+        });
     }
 }
