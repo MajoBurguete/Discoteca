4a9c3aa6a1b64a80dac8a4c4c41e6861e1e53ebe
Maria Burguete Euan <majoburguete@fb.com>
2021-07-12 22:37:05 -0500

    When someone logs in they can't go back to the login activity unless they log out #2

--- app/src/main/java/com/example/discoteca/LoginActivity.java
+++ app/src/main/java/com/example/discoteca/LoginActivity.java
@@ -59,5 +59,6 @@ public class LoginActivity extends AppCompatActivity {
     private void goMainActivity() {
         Intent i = new Intent(this, MainActivity.class);
         startActivity(i);
+        finish();
     }
 }
