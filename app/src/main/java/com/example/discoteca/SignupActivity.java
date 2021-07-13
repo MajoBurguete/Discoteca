4afb491fea8057b57d2bc0cfa175267b874d04fa
Maria Burguete Euan <majoburguete@fb.com>
2021-07-12 23:22:08 -0500

    A new user is created and the signup call is made to the data base #3

--- app/src/main/java/com/example/discoteca/SignupActivity.java
+++ app/src/main/java/com/example/discoteca/SignupActivity.java
@@ -3,12 +3,18 @@ package com.example.discoteca;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 import com.example.discoteca.databinding.ActivitySignupBinding;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class SignupActivity extends AppCompatActivity {
+
+    private String TAG = "SignupActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +32,23 @@ public class SignupActivity extends AppCompatActivity {
                     Toast.makeText(SignupActivity.this, "Make sure not to leave anything blank!", Toast.LENGTH_SHORT).show();
                     return;
                 }
+
+                //Create the ParseUser
+                ParseUser user = new ParseUser();
+
+                // Set properties
+                user.setUsername(username);
+                user.setPassword(password);
+                user.setEmail(email);
+
+                user.signUpInBackground(new SignUpCallback() {
+                    @Override
+                    public void done(ParseException e) {
+
+                    }
+                });
             }
+
         });
     }
 }
