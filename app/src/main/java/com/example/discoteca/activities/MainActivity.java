a1e6784fba7e5101e1d38e22f8e7e67ec820f7c1
María José Burguete Euán <66504153+MajoBurguete@users.noreply.github.com>
2021-07-14 18:10:12 -0500

    Logout button removed #12
    Logout button was removed from the main activity since now it's going to be on the menu.

--- app/src/main/java/com/example/discoteca/activities/MainActivity.java
+++ app/src/main/java/com/example/discoteca/activities/MainActivity.java
@@ -1,18 +1,18 @@
 package com.example.discoteca.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.discoteca.R;
 import com.parse.ParseUser;
-import okhttp3.OkHttpClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,24 +23,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btnLogout = findViewById(R.id.btnLogout);
-
         // Find the toolbar inside the layout
         Toolbar toolbar = findViewById(R.id.Toolbar);
         setSupportActionBar(toolbar);
-
-        // Logout from app
-        btnLogout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "AIUDA", Toast.LENGTH_SHORT).show();
-                ParseUser.logOut();
-                ParseUser currentUser = ParseUser.getCurrentUser();
-                Intent login = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(login);
-                finish();
-            }
-        });
     }
 
     @Override
@@ -48,4 +33,13 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return super.onCreateOptionsMenu(menu);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.btnLogout){
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
+
