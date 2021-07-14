f1ed00612ba0639b983a42d24b76600fe144ac56
María José Burguete Euán <66504153+MajoBurguete@users.noreply.github.com>
2021-07-14 18:02:53 -0500

    Main activity sets the support action bar #12
    Find the toolbar inside the layout and set it as the support action bar, plus the main menu is set too.

--- app/src/main/java/com/example/discoteca/activities/MainActivity.java
+++ app/src/main/java/com/example/discoteca/activities/MainActivity.java
@@ -1,16 +1,22 @@
 package com.example.discoteca.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.discoteca.R;
 import com.parse.ParseUser;
+import okhttp3.OkHttpClient;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,9 +25,15 @@ public class MainActivity extends AppCompatActivity {
 
         Button btnLogout = findViewById(R.id.btnLogout);
 
+        // Find the toolbar inside the layout
+        Toolbar toolbar = findViewById(R.id.Toolbar);
+        setSupportActionBar(toolbar);
+
+        // Logout from app
         btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "AIUDA", Toast.LENGTH_SHORT).show();
                 ParseUser.logOut();
                 ParseUser currentUser = ParseUser.getCurrentUser();
                 Intent login = new Intent(MainActivity.this, LoginActivity.class);
@@ -29,5 +41,11 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
