6dabde18a0fd268e96e78f7d04a544d8d5ef9281
María José Burguete Euán <66504153+MajoBurguete@users.noreply.github.com>
2021-07-15 10:11:27 -0500

    Album attributes #18

--- app/src/main/java/com/example/discoteca/models/Album.java
+++ app/src/main/java/com/example/discoteca/models/Album.java
@@ -1,4 +1,11 @@
 package com.example.discoteca.models;
 
 public class Album {
+
+    public String albumId;
+    public String albumName;
+    public int noTracks;
+    public String releaseDate;
+    public String imageUrl;
+
 }
