16ff33c0cfa7815efbf53dc4dd99b478180accc0
María José Burguete Euán <66504153+MajoBurguete@users.noreply.github.com>
2021-07-15 10:04:05 -0500

    Attributes defined #17
    Attributes for the song model where defined

--- app/src/main/java/com/example/discoteca/models/Song.java
+++ app/src/main/java/com/example/discoteca/models/Song.java
@@ -1,4 +1,12 @@
 package com.example.discoteca.models;
 
 public class Song {
+
+    public String songId;
+    public String songName;
+    public String albumName;
+    public String releaseDate;
+    public double duration;
+    public String imageUrl;
+    public String type;
 }
