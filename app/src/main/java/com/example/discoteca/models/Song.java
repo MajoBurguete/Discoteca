0773f33cdcb07ce0309c343ed0db83a70eb581d4
María José Burguete Euán <66504153+MajoBurguete@users.noreply.github.com>
2021-07-15 10:04:58 -0500

    Setters and Getters #17
    Setter and Getters were created for each attribute.

--- app/src/main/java/com/example/discoteca/models/Song.java
+++ app/src/main/java/com/example/discoteca/models/Song.java
@@ -9,4 +9,61 @@ public class Song {
     public double duration;
     public String imageUrl;
     public String type;
+
+    public String getSongId() {
+        return songId;
+    }
+
+    public void setSongId(String songId) {
+        this.songId = songId;
+    }
+
+    public String getSongName() {
+        return songName;
+    }
+
+    public void setSongName(String songName) {
+        this.songName = songName;
+    }
+
+    public String getAlbumName() {
+        return albumName;
+    }
+
+    public void setAlbumName(String albumName) {
+        this.albumName = albumName;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public double getDuration() {
+        return duration;
+    }
+
+    public void setDuration(double duration) {
+        this.duration = duration;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
 }
