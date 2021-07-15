1971d29081cc99072d3468522eb2c2cf907038ad
María José Burguete Euán <66504153+MajoBurguete@users.noreply.github.com>
2021-07-15 10:13:14 -0500

    Setters and Getters #18
    Setters and getters were defined for each attribute

--- app/src/main/java/com/example/discoteca/models/Album.java
+++ app/src/main/java/com/example/discoteca/models/Album.java
@@ -8,4 +8,48 @@ public class Album {
     public String releaseDate;
     public String imageUrl;
 
+    public String getAlbumId() {
+        return albumId;
+    }
+
+    public void setAlbumId(String albumId) {
+        this.albumId = albumId;
+    }
+
+    public String getAlbumName() {
+        return albumName;
+    }
+
+    public void setAlbumName(String albumName) {
+        this.albumName = albumName;
+    }
+
+    public int getNoTracks() {
+        return noTracks;
+    }
+
+    public void setNoTracks(int noTracks) {
+        this.noTracks = noTracks;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getType() {
+        return "album";
+    }
+    
 }
