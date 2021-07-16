cce3529d54eecc4484d74184511af96649a48c6b
María José Burguete Euán <66504153+MajoBurguete@users.noreply.github.com>
2021-07-16 13:50:24 -0500

    Variables initialized #19
    The array is created and the songs list is initialized. The song adapter is created and defined as well as the layout manager for the recycler view.

--- app/src/main/java/com/example/discoteca/fragments/SearchFragment.java
+++ app/src/main/java/com/example/discoteca/fragments/SearchFragment.java
@@ -6,6 +6,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
@@ -19,15 +20,22 @@ import com.example.discoteca.models.Album;
 import com.example.discoteca.models.Song;
 import com.google.android.material.tabs.TabLayout;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import okhttp3.OkHttpClient;
 
 public class SearchFragment extends Fragment {
 
+    private static final String TAG = "SearchFragment";
     RecyclerView rvSearch;
     SongAdapter songAdapter;
     List<Song> songs;
     List<Album> albums;
     SearchView searchBar;
+    private String accessToken;
+    TabLayout tabLayout;
+    private final OkHttpClient mOkHttpClient = new OkHttpClient();
 
     public SearchFragment() {
         // Required empty public constructor
@@ -45,12 +53,22 @@ public class SearchFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         // Tab layout functionality
-        TabLayout tabLayout = view.findViewById(R.id.tabLayout);
+        tabLayout = view.findViewById(R.id.tabLayout);
 
         searchBar = view.findViewById(R.id.searchBar);
 
         // Recycler view is defined
         rvSearch = view.findViewById(R.id.rvResults);
+
+        // Arrays are initialized
+        songs = new ArrayList<>();
+
+        // Adapter is created for each possible result
+        songAdapter = new SongAdapter(getContext(), songs);
+
+        // Adapter and layout manager is defined
+        rvSearch.setAdapter(songAdapter);
+        rvSearch.setLayoutManager(new LinearLayoutManager(getContext()));
 
         tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
@@ -60,11 +78,6 @@ public class SearchFragment extends Fragment {
                     String query = String.valueOf(searchBar.getQuery());
 
                     Toast.makeText(getContext(), "Tab " + query, Toast.LENGTH_LONG).show();
-
-                    // Adapter is created for each possible result
-                    songAdapter = new SongAdapter(getContext(), songs);
-                    // Adapter is defined
-                    rvSearch.setAdapter(songAdapter);
                 }
                 if (tabLayout.getSelectedTabPosition() == 1){
                     Toast.makeText(getContext(), "Tab 2 " + tabLayout.getSelectedTabPosition(), Toast.LENGTH_LONG).show();
