66b16df86ce23bec849fad3eb44e967e77824a15
María José Burguete Euán <66504153+MajoBurguete@users.noreply.github.com>
2021-07-19 14:41:35 -0500

    References were created from the album detail layout #22

--- app/src/main/java/com/example/discoteca/fragments/AlbumFragment.java
+++ app/src/main/java/com/example/discoteca/fragments/AlbumFragment.java
@@ -2,59 +2,29 @@ package com.example.discoteca.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.discoteca.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AlbumFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class AlbumFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    ImageView ivAlbumDet;
+    TextView tvAlbumNameD;
+    TextView tvArtistAlbum;
+    TextView tvAlbumYear;
+    RecyclerView rvAlbumSongs;
 
     public AlbumFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AlbumFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AlbumFragment newInstance(String param1, String param2) {
-        AlbumFragment fragment = new AlbumFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -62,5 +32,17 @@ public class AlbumFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_album, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        ivAlbumDet = view.findViewById(R.id.ivAlbumDet);
+        tvAlbumNameD = view.findViewById(R.id.tvAlbumNameD);
+        tvArtistAlbum = view.findViewById(R.id.tvArtistAlbum);
+        tvAlbumYear = view.findViewById(R.id.tvAlbumYear);
+        rvAlbumSongs = view.findViewById(R.id.rvAlbumSongs);
+
     }
 }
