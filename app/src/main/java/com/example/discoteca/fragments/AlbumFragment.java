6f036b29c589b035d129e631993a544fe2cd4f8c
María José Burguete Euán <66504153+MajoBurguete@users.noreply.github.com>
2021-07-19 18:19:39 -0500

    GET Album API Request#22
    Methods to get the tracks of the album picked were implemented.

--- app/src/main/java/com/example/discoteca/fragments/AlbumFragment.java
+++ app/src/main/java/com/example/discoteca/fragments/AlbumFragment.java
@@ -7,6 +7,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,10 +19,23 @@ import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 import com.example.discoteca.R;
 import com.example.discoteca.models.Album;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.parceler.Parcels;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class AlbumFragment extends Fragment {
 
+    public static final String TAG = "AlbumFragment";
     ImageView ivAlbumDet;
     TextView tvAlbumNameD;
     TextView tvArtistAlbum;
@@ -29,6 +43,7 @@ public class AlbumFragment extends Fragment {
     RecyclerView rvAlbumSongs;
     String accessToken;
     Album album;
+    private final OkHttpClient mOkHttpClient = new OkHttpClient();
 
     public AlbumFragment() {
         // Required empty public constructor
@@ -59,7 +74,52 @@ public class AlbumFragment extends Fragment {
         tvArtistAlbum.setText(album.getArtistName());
         tvAlbumYear.setText(album.getReleaseDate());
 
+        makeRequest(album.getAlbumId());
 
+
+    }
+
+    private void makeRequest(String id){
+
+        if (accessToken == null){
+            Log.e(TAG, "No token");
+            return;
+        }
+        String url = getUrl(id);
+
+        final Request request = new Request.Builder()
+                .url(url)
+                .addHeader("Authorization", "Bearer " + accessToken)
+                .build();
+
+        call(request);
+    }
+
+    private void call(Request request) {
+        mOkHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.e(TAG, "Failed to fetch data: " + e);
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                JSONObject jsonObject = null;
+                try {
+                    jsonObject = new JSONObject(response.body().string());
+                    JSONArray jsonArray = jsonObject.getJSONObject("tracks").getJSONArray("items");
+                    Log.i(TAG, "onResponse: " + jsonArray.toString());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private String getUrl(String id){
+        HttpUrl.Builder urlBuilder = HttpUrl.parse("https://api.spotify.com/v1/albums/"+id).newBuilder();
+        String url = urlBuilder.build().toString();
+        return url;
     }
 
 }
