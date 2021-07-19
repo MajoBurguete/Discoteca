8576d37c1b48952642cce4ca9dc7f75621514052
María José Burguete Euán <66504153+MajoBurguete@users.noreply.github.com>
2021-07-18 20:40:31 -0500

    Album adapter constructor was created #19

--- app/src/main/java/com/example/discoteca/adapters/AlbumAdapter.java
+++ app/src/main/java/com/example/discoteca/adapters/AlbumAdapter.java
@@ -1,12 +1,26 @@
 package com.example.discoteca.adapters;
 
+import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.discoteca.models.Album;
+
+import java.util.List;
+
 public class AlbumAdapter extends RecyclerView.Adapter<AlbumAdapter.ViewHolder> {
+
+    private Context context;
+    List<Album> rvAlbums;
+
+    public AlbumAdapter(Context context, List<Album> rvAlbums) {
+        this.context = context;
+        this.rvAlbums = rvAlbums;
+    }
+
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
