ffe3441f1d0d50efabddd0aa662e21ddda07ffa8
María José Burguete Euán <66504153+MajoBurguete@users.noreply.github.com>
2021-07-20 00:19:10 -0500

    Pick activity was created #23
    Activity to search and pick a song to write a fact about was created.

---
+++ app/src/main/java/com/example/discoteca/activities/PickActivity.java
@@ -0,0 +1,16 @@
+package com.example.discoteca.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.discoteca.R;
+
+public class PickActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pick);
+    }
+}
