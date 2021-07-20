246081ca621feab0507e148455718e30482197aa
María José Burguete Euán <66504153+MajoBurguete@users.noreply.github.com>
2021-07-20 01:57:08 -0500

    Access Token is stored on a variable #23
    In order to make a successful request i need to use the access key so i stored it.

--- app/src/main/java/com/example/discoteca/activities/PickActivity.java
+++ app/src/main/java/com/example/discoteca/activities/PickActivity.java
@@ -8,9 +8,13 @@ import com.example.discoteca.R;
 
 public class PickActivity extends AppCompatActivity {
 
+    private String accessToken;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pick);
+
+        accessToken = getIntent().getStringExtra("token");
     }
 }
