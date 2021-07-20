9acb7c08acb0ced84698ec6bb5fb1788a7bd87d7
María José Burguete Euán <66504153+MajoBurguete@users.noreply.github.com>
2021-07-20 00:52:53 -0500

    Views references from the compose layout #23

--- app/src/main/java/com/example/discoteca/activities/ComposeActivity.java
+++ app/src/main/java/com/example/discoteca/activities/ComposeActivity.java
@@ -3,14 +3,36 @@ package com.example.discoteca.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.discoteca.R;
 
 public class ComposeActivity extends AppCompatActivity {
 
+    ImageView ivComposeAlbum;
+    TextView tvAlbumName;
+    TextView tvArtistName;
+    TextView tvSongName;
+    Button btnPick;
+    EditText etDescription;
+    Button btnAddFact;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compose);
+
+        // Components references from compose layout
+        ivComposeAlbum = findViewById(R.id.ivComposeAlbum);
+        tvAlbumName = findViewById(R.id.tvAlbumName);
+        tvArtistName = findViewById(R.id.tvArtistName);
+        tvSongName = findViewById(R.id.tvSongName);
+        btnPick = findViewById(R.id.btnPick);
+        etDescription = findViewById(R.id.etDescriptionFact);
+        btnAddFact = findViewById(R.id.btnAddFact);
+
     }
 }
