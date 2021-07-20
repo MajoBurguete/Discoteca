616f58a00296aa88bd9b1a887622285de2a06787
María José Burguete Euán <66504153+MajoBurguete@users.noreply.github.com>
2021-07-20 02:26:39 -0500

    Components references from the pick layout #23

--- app/src/main/java/com/example/discoteca/activities/PickActivity.java
+++ app/src/main/java/com/example/discoteca/activities/PickActivity.java
@@ -1,14 +1,18 @@
 package com.example.discoteca.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.SearchView;
 
 import com.example.discoteca.R;
 
 public class PickActivity extends AppCompatActivity {
 
     private String accessToken;
+    SearchView searchBar;
+    RecyclerView rvPickSongs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +20,9 @@ public class PickActivity extends AppCompatActivity {
         setContentView(R.layout.activity_pick);
 
         accessToken = getIntent().getStringExtra("token");
+
+        // Components references from the pick activity layout
+        searchBar = findViewById(R.id.svPickSong);
+        rvPickSongs = findViewById(R.id.rvPickSongs);
     }
 }
