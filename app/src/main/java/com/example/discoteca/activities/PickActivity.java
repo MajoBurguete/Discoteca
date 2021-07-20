09a72f237b5d2e591cd46f2516dc3b9db971968a
María José Burguete Euán <66504153+MajoBurguete@users.noreply.github.com>
2021-07-20 02:50:52 -0500

    Variables for the recycler view were created #23
    List of song objects where the result will be stored. I also created the song adapter for the result recycler view.

--- app/src/main/java/com/example/discoteca/activities/PickActivity.java
+++ app/src/main/java/com/example/discoteca/activities/PickActivity.java
@@ -7,12 +7,18 @@ import android.os.Bundle;
 import android.widget.SearchView;
 
 import com.example.discoteca.R;
+import com.example.discoteca.adapters.SongAdapter;
+import com.example.discoteca.models.Song;
+
+import java.util.List;
 
 public class PickActivity extends AppCompatActivity {
 
     private String accessToken;
     SearchView searchBar;
     RecyclerView rvPickSongs;
+    List<Song> songs;
+    SongAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
