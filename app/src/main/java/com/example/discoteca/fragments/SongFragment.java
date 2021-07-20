d98427270402dc5169a5d2992776fbc4cbcde834
María José Burguete Euán <66504153+MajoBurguete@users.noreply.github.com>
2021-07-19 22:04:01 -0500

    Views references from the song detail layout #22

--- app/src/main/java/com/example/discoteca/fragments/SongFragment.java
+++ app/src/main/java/com/example/discoteca/fragments/SongFragment.java
@@ -2,59 +2,33 @@ package com.example.discoteca.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.example.discoteca.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SongFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class SongFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    public static final String TAG = "SongFragment";
+    ImageView ivSongDetail;
+    TextView tvNameDetail;
+    TextView tvArtistSong;
+    RecyclerView rvSongsFacts;
+    FloatingActionButton fabAdd;
+    RelativeLayout rlSong;
 
     public SongFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SongFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SongFragment newInstance(String param1, String param2) {
-        SongFragment fragment = new SongFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -62,5 +36,19 @@ public class SongFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_song, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Views references
+        ivSongDetail = view.findViewById(R.id.ivSongDetail);
+        tvNameDetail = view.findViewById(R.id.tvNameDetail);
+        tvArtistSong  = view.findViewById(R.id.tvArtistSong);
+        rvSongsFacts = view.findViewById(R.id.rvSongFacts);
+        fabAdd = view.findViewById(R.id.fabAdd);
+        rlSong = view.findViewById(R.id.rlSong);
+
     }
 }
