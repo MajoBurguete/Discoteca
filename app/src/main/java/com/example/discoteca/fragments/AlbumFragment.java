16b13a95da55735da90f243e93b9be77728a1f78
María José Burguete Euán <66504153+MajoBurguete@users.noreply.github.com>
2021-07-20 23:07:34 -0500

    Create Spotify client and make the request #25
    Moved request to the Spotify class. The request is now made through the spotify object.

--- app/src/main/java/com/example/discoteca/fragments/AlbumFragment.java
+++ app/src/main/java/com/example/discoteca/fragments/AlbumFragment.java
@@ -9,7 +9,6 @@ import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,25 +20,16 @@ import android.widget.TextView;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 import com.example.discoteca.R;
+import com.example.discoteca.Spotify;
 import com.example.discoteca.adapters.SongAdapter;
 import com.example.discoteca.models.Album;
 import com.example.discoteca.models.Song;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.parceler.Parcels;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.HttpUrl;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 
 public class AlbumFragment extends Fragment implements SongAdapter.OnSongClickListener{
 
@@ -55,7 +45,7 @@ public class AlbumFragment extends Fragment implements SongAdapter.OnSongClickLi
     String accessToken;
     Album album;
     SongAdapter adapter;
-    private final OkHttpClient mOkHttpClient = new OkHttpClient();
+    Spotify client;
 
     public AlbumFragment() {
         // Required empty public constructor
@@ -82,6 +72,7 @@ public class AlbumFragment extends Fragment implements SongAdapter.OnSongClickLi
 
         album = Parcels.unwrap(this.getArguments().getParcelable("album"));
         accessToken = this.getArguments().getString("token");
+        client = new Spotify(accessToken);
 
         Glide.with(getContext()).load(album.getImageUrl()).transform(new RoundedCorners(10)).into(ivAlbumDet);
         tvAlbumNameD.setText(album.getAlbumName());
@@ -105,70 +96,14 @@ public class AlbumFragment extends Fragment implements SongAdapter.OnSongClickLi
         rvAlbumSongs.setAdapter(adapter);
         rvAlbumSongs.setLayoutManager(new LinearLayoutManager(getContext()));
 
-        makeRequest(album.getAlbumId());
-
+        // Make API request
+        makeRequest();
 
     }
 
-    private void makeRequest(String id){
-
-        if (accessToken == null){
-            Log.e(TAG, "No token");
-            return;
-        }
-        String url = getUrl(id);
-
-        final Request request = new Request.Builder()
-                .url(url)
-                .addHeader("Authorization", "Bearer " + accessToken)
-                .build();
-
-        call(request);
-    }
-
-    private void call(Request request) {
-        mOkHttpClient.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-                Log.e(TAG, "Failed to fetch data: " + e);
-            }
-
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                JSONObject jsonObject = null;
-                try {
-                    jsonObject = new JSONObject(response.body().string());
-                    JSONArray jsonArray = jsonObject.getJSONObject("tracks").getJSONArray("items");
-                    for (int i = 0 ; i < jsonArray.length(); i++){
-                        Song song = new Song();
-                        JSONObject responseObject = jsonArray.getJSONObject(i);
-                        song.setAlbumName(album.getAlbumName());
-                        song.setArtistName(album.getArtistName());
-                        song.setImageUrl(album.getImageUrl());
-                        song.setReleaseDate(album.getReleaseDate());
-                        song.setSongName(responseObject.getString("name"));
-                        song.setSongId(responseObject.getString("id"));
-                        song.setDuration(responseObject.getLong("duration_ms"));
-
-                        songList.add(song);
-                    }
-                    getActivity().runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            adapter.notifyDataSetChanged();
-                        }
-                    });
-                } catch (JSONException e) {
-                    Log.e(TAG, "Failed to parse data: " + e);
-                }
-            }
-        });
-    }
-
-    private String getUrl(String id){
-        HttpUrl.Builder urlBuilder = HttpUrl.parse("https://api.spotify.com/v1/albums/"+id).newBuilder();
-        String url = urlBuilder.build().toString();
-        return url;
+    private void makeRequest() {
+        List<Song> spotifyResp = client.makeAlbumRequest(album);
+        adapter.addAll(spotifyResp);
     }
 
     @Override
