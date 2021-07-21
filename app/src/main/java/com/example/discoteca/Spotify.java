869706ad461db110407d55e3c5e455c4f5ad3b61
María José Burguete Euán <66504153+MajoBurguete@users.noreply.github.com>
2021-07-21 00:02:43 -0500

    Search album API request #25
    Methods to make the search an album API request were implemented. It receives a query and returns a list of albums that matches the query.

--- app/src/main/java/com/example/discoteca/Spotify.java
+++ app/src/main/java/com/example/discoteca/Spotify.java
@@ -30,6 +30,7 @@ public class Spotify {
     String accessToken;
     Album album;
     List<Song> songList = new ArrayList<>();
+    List<Album> albumList = new ArrayList<>();
 
     public Spotify(String accessToken) {
         this.accessToken = accessToken;
@@ -66,6 +67,68 @@ public class Spotify {
         return songList;
     }
 
+    public List<Album> makeSearchAlbumRequest(String query){
+
+        if (accessToken == null){
+            Log.e(TAG, "No token");
+        }
+        String url = getSearchUrl(query, "album");
+
+        final Request request = new Request.Builder()
+                .url(url)
+                .addHeader("Authorization", "Bearer " + accessToken)
+                .build();
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                callAlbum(request);
+            }
+        };
+
+        // Creates a new thread to wait for the API response
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        executorService.execute(runnable);
+        try {
+            executorService.awaitTermination(500, TimeUnit.MILLISECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return albumList;
+    }
+
+    private void callAlbum(Request request) {
+        mOkHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.e(TAG, "Failed to fetch data: " + e);
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+                    JSONObject jsonObject = new JSONObject(response.body().string());
+                    JSONArray jsonArray = jsonObject.getJSONObject("albums").getJSONArray("items");
+                    for (int i = 0; i < jsonArray.length(); i++){
+                        Album albumR = new Album();
+                        JSONObject album = jsonArray.getJSONObject(i);
+                        albumR.setAlbumId(album.getString("id"));
+                        albumR.setAlbumName(album.getString("name"));
+                        albumR.setArtistName(album.getJSONArray("artists").getJSONObject(0).getString("name"));
+                        albumR.setImageUrl(album.getJSONArray("images").getJSONObject(1).getString("url"));
+                        albumR.setNoTracks(album.getInt("total_tracks"));
+                        albumR.setReleaseDate(album.getString("release_date"));
+                        albumList.add(albumR);
+                    }
+
+                } catch (JSONException e) {
+                    Log.e(TAG, "Failed to parse data: " + e);
+                }
+
+            }
+        });
+    }
 
     private void callSong(Request request) {
         mOkHttpClient.newCall(request).enqueue(new Callback() {
