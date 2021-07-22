0f1cddcb035b81f073cdd5522796fd6968fbeb78
María José Burguete Euán <66504153+MajoBurguete@users.noreply.github.com>
2021-07-22 14:26:52 -0500

    Fact adapter class was created #36
    Adapter to handle fact objects.

---
+++ app/src/main/java/com/example/discoteca/adapters/FactAdapter.java
@@ -0,0 +1,29 @@
+package com.example.discoteca.adapters;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class FactAdapter extends RecyclerView.Adapter<FactAdapter.ViewHolder> {
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FactAdapter.ViewHolder holder, int position) {
+    }
+
+    @Override
+    public int getItemCount() {
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
