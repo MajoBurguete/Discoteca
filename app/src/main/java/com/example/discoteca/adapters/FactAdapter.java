873a8c8c4758a6a77c05d15db1e01308046ba4e7
María José Burguete Euán <66504153+MajoBurguete@users.noreply.github.com>
2021-07-22 14:31:05 -0500

    Fact object class constructor was created #36
    It receives the context, the list of facts to bind into the recycler view, the Spotify client to make the song by id request and the activity to update the UI once the API has given a response.

--- app/src/main/java/com/example/discoteca/adapters/FactAdapter.java
+++ app/src/main/java/com/example/discoteca/adapters/FactAdapter.java
@@ -1,12 +1,32 @@
 package com.example.discoteca.adapters;
 
+import android.app.Activity;
+import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.discoteca.Spotify;
+import com.example.discoteca.models.Fact;
+import com.example.discoteca.models.Song;
+import java.util.List;
 public class FactAdapter extends RecyclerView.Adapter<FactAdapter.ViewHolder> {
+
+    private static final String TAG = "FactAdapter";
+    private Context context;
+    private Activity activity;
+    List<Fact> rvFact;
+    Spotify client;
+    Song song;
+
+    public FactAdapter(Context context, List<Fact> rvFact, Spotify client, Activity activity){
+        this.context = context;
+        this.rvFact = rvFact;
+        this.client = client;
+        this.activity = activity;
+    }
 
     @NonNull
     @Override
