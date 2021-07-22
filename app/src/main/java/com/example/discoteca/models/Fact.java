4807518fd654d963abafa5d317855080055722b8
María José Burguete Euán <66504153+MajoBurguete@users.noreply.github.com>
2021-07-22 13:07:44 -0500

    Setters and Getters for the Fact Class #33

--- app/src/main/java/com/example/discoteca/models/Fact.java
+++ app/src/main/java/com/example/discoteca/models/Fact.java
@@ -2,6 +2,7 @@ package com.example.discoteca.models;
 
 import com.parse.ParseClassName;
 import com.parse.ParseObject;
+import com.parse.ParseUser;
 
 @ParseClassName("Fact")
 public class Fact  extends ParseObject {
@@ -10,4 +11,27 @@ public class Fact  extends ParseObject {
     public static final String KEY_DESCRIPTION = "description";
     public static final String KEY_SONG = "songId";
 
+    public ParseUser getUser() {
+        return getParseUser(KEY_USER);
+    }
+
+    public void setUser(ParseUser user){
+        put(KEY_USER, user);
+    }
+
+    public String getDescription() {
+        return getString(KEY_DESCRIPTION);
+    }
+
+    public void setDescription(String description){
+        put(KEY_DESCRIPTION, description);
+    }
+
+    public String getSong() {
+        return getString(KEY_SONG);
+    }
+
+    public void setSong(String id){
+        put(KEY_SONG, id);
+    }
 }
