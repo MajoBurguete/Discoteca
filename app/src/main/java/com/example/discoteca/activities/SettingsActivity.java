c0081650ee7507542fc05c369798d16005e927e4
María José Burguete Euán <66504153+MajoBurguete@users.noreply.github.com>
2021-07-23 11:50:44 -0500

    Save button on click listener #37
    Once the save button is clicked, the new username written is stored on a string and i also stored the current user to update its username.

--- app/src/main/java/com/example/discoteca/activities/SettingsActivity.java
+++ app/src/main/java/com/example/discoteca/activities/SettingsActivity.java
@@ -3,17 +3,25 @@ package com.example.discoteca.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.discoteca.R;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 public class SettingsActivity extends AppCompatActivity {
 
+    public static final String KEY_USERNAME = "username";
+    private String TAG = "SettingsActivity";
     ImageView ivUserPict;
     EditText etNameEdit;
     TextView tvEmail;
@@ -38,6 +46,14 @@ public class SettingsActivity extends AppCompatActivity {
         etNameEdit.setText(user.getUsername());
         tvEmail.setText(user.getEmail());
 
+        btnSaveProf.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ParseUser currUser = ParseUser.getCurrentUser();
+                String newUsername = etNameEdit.getText().toString();
+
+            }
+        });
 
     }
 }
