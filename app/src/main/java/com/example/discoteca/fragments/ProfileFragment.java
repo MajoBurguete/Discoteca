7a724778e4e0f8a29a8d85d8c208cfe09e1f0e54
María José Burguete Euán <66504153+MajoBurguete@users.noreply.github.com>
2021-07-23 12:25:20 -0500

    Layout references #39
    References to the views or components were created.

--- app/src/main/java/com/example/discoteca/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/discoteca/fragments/ProfileFragment.java
@@ -2,16 +2,28 @@ package com.example.discoteca.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.discoteca.R;
 
 
 public class ProfileFragment extends Fragment {
+
+    private String TAG = "ProfileFragment";
+    ImageView ivProfPict;
+    TextView tvNameP;
+    TextView tvNumber;
+    RecyclerView rvUserFacts;
+    public static final String KEY_PROFILE = "profile";
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -23,4 +35,15 @@ public class ProfileFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Layout references
+        ivProfPict = view.findViewById(R.id.ivProfPict);
+        tvNameP = view.findViewById(R.id.tvNameP);
+        tvNumber = view.findViewById(R.id.tvNumber);
+        rvUserFacts = view.findViewById(R.id.rvUserFacts);
+
 }
