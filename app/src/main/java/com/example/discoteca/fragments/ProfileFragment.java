c2e39ad20f9d876fb53f14c58637a4c2db2949bd
María José Burguete Euán <66504153+MajoBurguete@users.noreply.github.com>
2021-07-23 12:48:30 -0500

    Adapter and layout manager assigned to the recycler view #39

--- app/src/main/java/com/example/discoteca/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/discoteca/fragments/ProfileFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.util.Log;
@@ -70,6 +71,10 @@ public class ProfileFragment extends Fragment {
 
         // Adapter initialized
         adapter = new FactAdapter(getContext(), userFacts);
+
+        // Assign adapter and layout manager
+        rvUserFacts.setAdapter(adapter);
+        rvUserFacts.setLayoutManager(new LinearLayoutManager(getContext()));
 
         queryFacts();
 
