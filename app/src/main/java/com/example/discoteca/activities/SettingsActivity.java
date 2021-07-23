fe029e2699fb720ebc3e1fea47a9775847e355d4
María José Burguete Euán <66504153+MajoBurguete@users.noreply.github.com>
2021-07-23 03:29:45 -0500

    Attributes from the settings layout were created #37

--- app/src/main/java/com/example/discoteca/activities/SettingsActivity.java
+++ app/src/main/java/com/example/discoteca/activities/SettingsActivity.java
@@ -3,8 +3,21 @@ package com.example.discoteca.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.discoteca.R;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    ImageView ivUserPict;
+    EditText etNameEdit;
+    TextView tvEmail;
+    Button btnSaveProf;
+    ImageButton ibClose;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
