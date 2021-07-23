ae9baf45fb52c156ff007ab5e0ca74b62e924b0e
María José Burguete Euán <66504153+MajoBurguete@users.noreply.github.com>
2021-07-23 02:43:42 -0500

    Modified fact model #34
    Instead of just saving the song's id now i save the image url, the song name, the album name and the artist name. In this way i don't have to do a request every time the home feed appears.

--- app/src/main/java/com/example/discoteca/models/Fact.java
+++ app/src/main/java/com/example/discoteca/models/Fact.java
@@ -9,7 +9,11 @@ public class Fact  extends ParseObject {
 
     public static final String KEY_USER = "userFact";
     public static final String KEY_DESCRIPTION = "description";
-    public static final String KEY_SONG = "songId";
+    public static final String KEY_ID = "songId";
+    public static final String KEY_URL = "url";
+    public static final String KEY_SONG = "song";
+    public static final String KEY_ALBUM = "album";
+    public static final String KEY_ARTIST = "artist";
 
     public ParseUser getUser() {
         return getParseUser(KEY_USER);
@@ -27,11 +31,27 @@ public class Fact  extends ParseObject {
         put(KEY_DESCRIPTION, description);
     }
 
-    public String getSong() {
-        return getString(KEY_SONG);
+    public String getId() {
+        return getString(KEY_ID);
     }
 
-    public void setSong(String id){
-        put(KEY_SONG, id);
+    public void setId(String id){
+        put(KEY_ID, id);
     }
+
+    public String getUrl(){ return getString(KEY_URL); }
+
+    public void setUrl(String url){ put(KEY_URL, url); }
+
+    public String getSong(){ return getString(KEY_SONG); }
+
+    public void setSong(String song){ put(KEY_SONG, song); }
+
+    public String getAlbum(){ return getString(KEY_ALBUM); }
+
+    public void setAlbum(String album){ put(KEY_ALBUM, album); }
+
+    public String getArtist(){ return getString(KEY_ARTIST); }
+
+    public void setArtist(String artist){ put(KEY_ARTIST, artist); }
 }
