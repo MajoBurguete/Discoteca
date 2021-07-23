4aa61ead70d50255fba6acd92321e337550fc1b3
María José Burguete Euán <66504153+MajoBurguete@users.noreply.github.com>
2021-07-23 14:59:06 -0500

    Update profile picture #41
    Once the username has been saved, the image is sent and saved on the database, the image view on the settings activity also gets updated.

--- app/src/main/java/com/example/discoteca/activities/SettingsActivity.java
+++ app/src/main/java/com/example/discoteca/activities/SettingsActivity.java
@@ -155,6 +155,16 @@ public class SettingsActivity extends AppCompatActivity {
                     Log.e(TAG, "Error while saving description", e);
                     Toast.makeText(SettingsActivity.this, "Error while saving!", Toast.LENGTH_SHORT).show();
                 }
+                currentUser.put(KEY_PROFILE, photoFile);
+                if (photoFile != null){
+                    currentUser.saveInBackground(new SaveCallback() {
+                        @Override
+                        public void done(ParseException e) {
+                            Glide.with(context).load(currentUser.getParseFile(KEY_PROFILE).getUrl()).circleCrop().into(ivUserPict);
+                            Toast.makeText(SettingsActivity.this, "Your picture was save successfully", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
                 Toast.makeText(SettingsActivity.this, "Your username was save successfully", Toast.LENGTH_SHORT).show();
             }
         });
