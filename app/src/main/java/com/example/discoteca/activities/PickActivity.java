6a2f4cf8a6303a60745eea72f4e09d28e4b88a8f
María José Burguete Euán <66504153+MajoBurguete@users.noreply.github.com>
2021-07-27 11:30:19 -0500

    Offset attribute on the pick activity #54
    I notice that the API gives 20 objects and the next set are the next 20 objects, in order to keep giving different results when the user scrolls down i have to multiply the page where the adapter is on by 20 in order to get the next set of the objects from the API. The first 20 objects are gotten with 0, the next set is gotten on the page 1 and the offset value is 20*1 = 20, on the page 2 is 40 and so it goes.

--- app/src/main/java/com/example/discoteca/activities/PickActivity.java
+++ app/src/main/java/com/example/discoteca/activities/PickActivity.java
@@ -74,6 +74,7 @@ public class PickActivity extends AppCompatActivity implements SongAdapter.OnSon
             public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
                 // Triggered only when new data needs to be appended to the list
                 // Add whatever code is needed to append new items to the bottom of the list
+                page = page * 20;
                 loadNextDataFromApi(page);
             }
         };
