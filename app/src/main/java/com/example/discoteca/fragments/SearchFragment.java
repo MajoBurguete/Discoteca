d0449c8f12d58352ca00342ac163ab6005e60a63
María José Burguete Euán <66504153+MajoBurguete@users.noreply.github.com>
2021-07-27 11:28:55 -0500

    Offset attribute #53
    I notice that the API gives 20 objects and the next set are the next 20 objects, in order to keep giving different results when the user scrolls down i have to multiply the page where the adapter is on by 20 in order to get the next set of the objects from the API. The first 20 objects are gotten with 0, the next set is gotten on the page 1 and the offset value is 20*1 = 20, on the page 2 is 40 and so it goes.

--- app/src/main/java/com/example/discoteca/fragments/SearchFragment.java
+++ app/src/main/java/com/example/discoteca/fragments/SearchFragment.java
@@ -93,6 +93,7 @@ public class SearchFragment extends Fragment implements AlbumAdapter.OnAlbumClic
             public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
                 // Triggered only when new data needs to be appended to the list
                 // Add whatever code is needed to append new items to the bottom of the list
+                page = page * 20;
                 loadNextDataFromApi(page);
             }
         };
