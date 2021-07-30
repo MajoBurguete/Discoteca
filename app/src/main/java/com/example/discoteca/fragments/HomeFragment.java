f956434de34f1a45c55b39ed26007e99764b1894
María José Burguete Euán <66504153+MajoBurguete@users.noreply.github.com>
2021-07-30 12:55:13 -0500

    For statement moved #65
    The for statement should only happen if the list size is greater than 0 so i had to move it to the else statement.

--- app/src/main/java/com/example/discoteca/fragments/HomeFragment.java
+++ app/src/main/java/com/example/discoteca/fragments/HomeFragment.java
@@ -131,40 +131,7 @@ public class HomeFragment extends Fragment implements FactAdapter.OnFactClickLis
         Fact fact = factsL.get(position);
         String objectID = fact.getObjectId();
         int likes = fact.getLikes();
-
-        for (int i = 0; i < likeFacts.size(); i++){
-            if (likeFacts.get(i).equals(objectID)){
-                Toast.makeText(getContext(), "Liked already", Toast.LENGTH_SHORT).show();
-                likeFacts.remove(i);
-
-                // Update number of likes on the fact
-                likes = likes - 1;
-                fact.setLikes(likes);
-                fact.saveInBackground(new SaveCallback() {
-                    @Override
-                    public void done(ParseException e) {
-                        user.put(KEY_LIST, likeFacts);
-                        saveUser(user);
-                    }
-                });
-
-                break;
-            } else if (i == likeFacts.size()-1){
-                Toast.makeText(getContext(), "Liking ...", Toast.LENGTH_SHORT).show();
-                likeFacts.add(0, fact.getObjectId());
-
-                // Update number of likes on the fact
-                likes =+ 1;
-                fact.setLikes(likes);
-                fact.saveInBackground(new SaveCallback() {
-                    @Override
-                    public void done(ParseException e) {
-                        user.put(KEY_LIST, likeFacts);
-                        saveUser(user);
-                    }
-                });
-
-                break;
+        Toast.makeText(getContext(), "Likes " + likes, Toast.LENGTH_SHORT).show();
         int likeCheck = likeFacts.size();
         if (likeCheck == 0){
             Toast.makeText(getContext(), "Liking ...", Toast.LENGTH_SHORT).show();
