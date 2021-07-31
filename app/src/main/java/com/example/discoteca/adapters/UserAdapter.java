377b0499e2aade78f1399e31eef295844f1f3987
María José Burguete Euán <66504153+MajoBurguete@users.noreply.github.com>
2021-07-30 21:03:33 -0500

    Adapter constructor #71
    It receives the context and the list of parse users.

--- app/src/main/java/com/example/discoteca/adapters/UserAdapter.java
+++ app/src/main/java/com/example/discoteca/adapters/UserAdapter.java
@@ -1,7 +1,21 @@
 package com.example.discoteca.adapters;
+import android.content.Context;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+import com.parse.ParseUser;
+
+import java.util.List;
+
 public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
+
+    private Context context;
+    List<ParseUser> rvUsers;
+
+    public UserAdapter(Context context, List<ParseUser> rvUsers) {
+        this.context = context;
+        this.rvUsers = rvUsers;
+    }
+
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
