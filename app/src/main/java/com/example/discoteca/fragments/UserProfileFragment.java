2c1ec9e58de6e25243107da59f546ab01b54b8d4
María José Burguete Euán <66504153+MajoBurguete@users.noreply.github.com>
2021-07-31 01:31:17 -0500

    References from user profile layout #72

--- app/src/main/java/com/example/discoteca/fragments/UserProfileFragment.java
+++ app/src/main/java/com/example/discoteca/fragments/UserProfileFragment.java
@@ -5,14 +5,32 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.discoteca.R;
+import com.example.discoteca.adapters.FactAdapter;
+import com.example.discoteca.models.Fact;
+import com.parse.ParseUser;
 
 public class UserProfileFragment extends Fragment {
+import java.util.List;
+
+    private static final String TAG = "UserProfileFragment";
+    TextView tvUserName;
+    ImageView ivOtherUserPict;
+    SwipeRefreshLayout srUserProfile;
+    RecyclerView rvUserFacts;
+    List<Fact> userFacts;
+    FactAdapter factAdapter;
+    TextView tvUserNumber;
+    ParseUser user;
 
     public UserProfileFragment() {
         // Required empty public constructor
@@ -28,5 +46,12 @@ public class UserProfileFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        // Layout references
+        tvUserName = view.findViewById(R.id.tvUserName);
+        ivOtherUserPict = view.findViewById(R.id.ivOtherUserPict);
+        srUserProfile = view.findViewById(R.id.srUserProfile);
+        rvUserFacts = view.findViewById(R.id.rvOtherUserFacts);
+        tvUserNumber = view.findViewById(R.id.tvUserNumber);
     }
 }
