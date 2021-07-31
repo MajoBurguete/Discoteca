3ecd27e5078f60b8935b3371d7aee55123d96b91
María José Burguete Euán <66504153+MajoBurguete@users.noreply.github.com>
2021-07-30 20:53:20 -0500

    User adapter class was created #71

---
+++ app/src/main/java/com/example/discoteca/adapters/UserAdapter.java
@@ -0,0 +1,25 @@
+package com.example.discoteca.adapters;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UserAdapter.ViewHolder holder, int position) {
+    public void clearAll(boolean notify){
+    }
+
+    public void addAll(List<ParseUser> users){
+    }
+
+    @Override
+    public int getItemCount() {
+    }
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+}
