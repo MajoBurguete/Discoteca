ab614050a0d35e8ba57e0dcde0ec1206c8177cfc
María José Burguete Euán <66504153+MajoBurguete@users.noreply.github.com>
2021-08-01 13:53:26 -0500

    Get fact object on the list of facts made by the user #73
    With this object i get their object id and the number of likes on the fact.

--- app/src/main/java/com/example/discoteca/fragments/UserProfileFragment.java
+++ app/src/main/java/com/example/discoteca/fragments/UserProfileFragment.java
@@ -134,6 +134,9 @@ public class UserProfileFragment extends Fragment implements FactAdapter.OnFactC
     public void onLikeClick(int position) {
         ParseUser user = ParseUser.getCurrentUser();
         List<String> likeFacts= user.getList(KEY_LIST);
+        Fact fact = userFacts.get(position);
+        String objectID = fact.getObjectId();
+        int likes = fact.getLikes();
     }
 
     @Override
