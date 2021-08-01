62161ea3614741fb3d6abfecf49aa07640c8f3ec
María José Burguete Euán <66504153+MajoBurguete@users.noreply.github.com>
2021-08-01 14:01:11 -0500

    Fact being disliked #73
    If the fact was already on the list, that means the user is disliking it so i update the number of likes and update the fact on the database, once this is done i update the list of facts liked by the user and save the user into the database to update their info.

--- app/src/main/java/com/example/discoteca/fragments/UserProfileFragment.java
+++ app/src/main/java/com/example/discoteca/fragments/UserProfileFragment.java
@@ -153,6 +153,22 @@ public class UserProfileFragment extends Fragment implements FactAdapter.OnFactC
             });
         } else{
             for (int i = 0; i < likeFacts.size(); i++){
+                if (likeFacts.get(i).equals(objectID)){
+                    Toast.makeText(getContext(), "Liked already", Toast.LENGTH_SHORT).show();
+                    likeFacts.remove(i);
+
+                    // Update number of likes on the fact
+                    likes = likes - 1;
+                    fact.setLikes(likes);
+                    fact.saveInBackground(new SaveCallback() {
+                        @Override
+                        public void done(ParseException e) {
+                            user.put(KEY_LIST, likeFacts);
+                            saveUser(user);
+                        }
+                    });
+
+                    break;
             }
         }
     }
