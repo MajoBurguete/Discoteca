87384ec9eee7d2716bec1a425e3bb6db92d5dd77
María José Burguete Euán <66504153+MajoBurguete@users.noreply.github.com>
2021-08-01 20:55:12 -0500

    Button references from layout #77

--- app/src/main/java/com/example/discoteca/fragments/UserProfileFragment.java
+++ app/src/main/java/com/example/discoteca/fragments/UserProfileFragment.java
@@ -14,6 +14,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -46,6 +47,7 @@ public class UserProfileFragment extends Fragment implements FactAdapter.OnFactC
     FactAdapter factAdapter;
     TextView tvUserNumber;
     TextView tvFriendsNumber;
+    Button btnFollow;
     ParseUser user;
 
     public UserProfileFragment() {
@@ -70,6 +72,7 @@ public class UserProfileFragment extends Fragment implements FactAdapter.OnFactC
         rvUserFacts = view.findViewById(R.id.rvOtherUserFacts);
         tvUserNumber = view.findViewById(R.id.tvUserNumber);
         tvFriendsNumber = view.findViewById(R.id.tvFriendsNumber);
+        btnFollow = view.findViewById(R.id.btnFollow);
 
         // user gotten from arguments
         user = Parcels.unwrap(this.getArguments().getParcelable("user"));
