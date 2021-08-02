61a5ed1a09c17cb5826de15eb94008d90e1fff22
María José Burguete Euán <66504153+MajoBurguete@users.noreply.github.com>
2021-08-01 21:01:51 -0500

    checkIfFriend method was created #77
    Method that returns a boolean that indicates if the user is already followed by the current user.

--- app/src/main/java/com/example/discoteca/fragments/UserProfileFragment.java
+++ app/src/main/java/com/example/discoteca/fragments/UserProfileFragment.java
@@ -253,6 +253,8 @@ public class UserProfileFragment extends Fragment implements FactAdapter.OnFactC
     public void addFriend(){
         ParseUser currentUser = ParseUser.getCurrentUser();
         List<ParseUser> friends = currentUser.getList(FRIENDS_LIST_KEY);
+        boolean boolFriend = checkIfFriend();
+    private boolean checkIfFriend(){
 
     }
 }
