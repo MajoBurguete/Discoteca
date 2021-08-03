4795aa7c300fc5e519f65e9e511d72d6f24ae4ef
María José Burguete Euán <66504153+MajoBurguete@users.noreply.github.com>
2021-08-03 00:07:52 -0500

    Layout references #85

--- app/src/main/java/com/example/discoteca/fragments/FriendsFragment.java
+++ app/src/main/java/com/example/discoteca/fragments/FriendsFragment.java
@@ -1,4 +1,10 @@
 package com.example.discoteca.fragments;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
 public class FriendsFragment extends Fragment implements UserAdapter.OnUserListener{
 
     public FriendsFragment() {
@@ -6,6 +12,8 @@ public class FriendsFragment extends Fragment implements UserAdapter.OnUserListe
     }
 
     private static final String TAG = "FriendFragment";
+    RecyclerView rvUsers;
+    ImageButton ibReturn;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -16,6 +24,10 @@ public class FriendsFragment extends Fragment implements UserAdapter.OnUserListe
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        // Layout references
+        rvUsers = view.findViewById(R.id.rvFriends);
+        ibReturn = view.findViewById(R.id.ibReturnProfile);
 
     }
 }
