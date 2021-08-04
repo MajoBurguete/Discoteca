3d5719eebf3f1e12b6d0486bc87cf1acdaf71ccf
María José Burguete Euán <66504153+MajoBurguete@users.noreply.github.com>
2021-08-03 21:13:10 -0500

    Spotify variables #96
    Since this will be the main activity i decided to make the token request here

--- app/src/main/java/com/example/discoteca/activities/WelcomeActivity.java
+++ app/src/main/java/com/example/discoteca/activities/WelcomeActivity.java
@@ -8,6 +8,21 @@ import com.example.discoteca.R;
 
 public class WelcomeActivity extends AppCompatActivity {
 
+    private static final int REQUEST_CODE_SIGN = 7;
+    private static final String CLIENT_ID = BuildConfig.CONSUMER_KEY;
+    public static final String CLIENT_SECRET = BuildConfig.CONSUMER_SECRET;
+    private static final String REDIRECT_URI = "https://example.com/spotify-redirect";
+    private static final URI redirectUri = SpotifyHttpManager.makeUri(REDIRECT_URI);
+    private String accessToken = "";
+    private static final SpotifyApi spotifyApi = new SpotifyApi.Builder()
+            .setClientId(CLIENT_ID)
+            .setClientSecret(CLIENT_SECRET)
+            .setRedirectUri(redirectUri)
+            .build();
+
+    private static final ClientCredentialsRequest clientCredentialsRequest = spotifyApi.clientCredentials()
+            .build();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
